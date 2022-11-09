7d17c58d8294d3c2829b119ccf94f893bbeb6e80
marcosmichel89 <mmartinez@octupus.es>
2022-11-08 21:39:13 -0500

    added computed function to mark profile completed when all social url are registered

--- cengineer_tp/models/res_partner.py
+++ cengineer_tp/models/res_partner.py
@@ -11,4 +11,11 @@ class ResPartner(models.Model):
     url_facebook = fields.Char(string='Facebook')
     url_linkedin = fields.Char(string='LinkedIn')
     url_twitter = fields.Char(string='Twitter')
+    complete_profile = fields.Boolean(string='Complete Profile', readonly=True, compute='_compute_profile')
 
+    def _compute_profile(self):
+        for partner in self:
+            if not partner.url_facebook or not partner.url_twitter or not partner.url_linkedin:
+                partner.complete_profile = False
+            else:
+                partner.complete_profile = True
